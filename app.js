89fc7dd0fca3a43464448d1696eebb54af08ee9a
Manasi Rane <madhu12rane@gmail.com>
2019-01-27 14:18:03 +0530

    change

---
+++ app.js
@@ -0,0 +1,47 @@
+var Hapi = require('hapi');
+var mysql= require('mysql');
+var connection = mysql.createConnection({
+host     : 'hapi-mysql.c0iqz6ndca9y.ap-south-1.rds.amazonaws.com',
+user     : 'root',
+password : 'madhura.rane',
+port     : '3306',
+database : 'hapi_db'
+});
+var server = new Hapi.Server();
+server.connection({ port: process.env.PORT || 8080 });
+connection.connect();
+server.register(require('inert'));
+server.register(require('vision'),function(err){
+if(err){
+throw err;
+}
+server.route({
+method: 'GET',
+path: '/',
+handler: function (request, reply) {
+connection.query('SELECT quote,credit from quotes order by rand() limit 1', function(err, rows, fields) {
+if (err) throw err;
+reply.view('index',{quote:rows[0].quote,credit:rows[0].credit});
+});
+}
+});
+server.route({
+method: 'GET',
+path: '/{param*}',
+handler: {
+directory: {
+path: 'public'
+}
+}
+});
+server.views({
+engines: {
+html: require('handlebars')
+},
+relativeTo: __dirname,
+path: 'templates'
+});
+});
+server.start( function(){
+console.log('Catch the action at : '+server.info.uri);
+});
